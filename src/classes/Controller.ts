1eff525edbc41e3fba9a64fd62a327ba734a60ca
Tsuyoshi Goto <shprintuzt@gmail.com>
2022-04-16 16:35:43 +0900

    Add Controller and its test.

---
+++ src/classes/Controller.ts
@@ -0,0 +1,52 @@
+import { Board } from "./Board";
+import { Piece } from "./PieceType";
+
+export const Mode = {
+    None: 0,
+    Normal: 1,
+} as const;
+
+export type ModeType = typeof Mode[keyof typeof Mode];
+
+
+export class Controller {
+    _board: Board;
+    _playing: boolean;
+    _score: number;
+    _totalErasedRowNum: number;
+    _mode: ModeType;
+
+    constructor() {
+        this._board = new Board(10, 30);
+        this._playing = false;
+        this._score = 0;
+        this._totalErasedRowNum = 0;
+        this._mode = Mode.None;
+    }
+
+    get board() {return this._board;}
+    get playing() {return this._playing;}
+    get score() {return this._score;}
+    get totalErasedRowNum() {return this._totalErasedRowNum;}
+    get mode() {return this._mode}
+
+    startNormal = () => {
+        this._mode = Mode.Normal;
+        this._board.clearBoard(10, 30);
+        this.start()
+    }
+
+    start = () => {
+        this.board.newCurrentPiece(Piece.O);
+        this._board.updateBoard()
+        this._playing = true
+        this._score = 0
+        this._totalErasedRowNum = 0
+    }
+
+// startErasing = () => {
+    //     this._playing = true;
+    //     this._mode = 'erasing';
+    // }
+
+}
---
+++ src/classes/__test__/Controller.test.ts
@@ -0,0 +1,19 @@
+import { Controller, Mode } from "../Controller";
+
+describe('Controller test', () => {
+    test('create controller', () => {
+        let controller = new Controller();
+        expect(controller.playing).toBe(false);
+        expect(controller.score).toBe(0);
+        expect(controller.totalErasedRowNum).toBe(0);
+        expect(controller.mode).toBe(Mode.None);
+    });
+    test('start normal game', () => {
+        let controller = new Controller();
+        controller.startNormal()
+        expect(controller.playing).toBe(true);
+        expect(controller.score).toBe(0);
+        expect(controller.totalErasedRowNum).toBe(0);
+        expect(controller.mode).toBe(Mode.Normal);
+    });
+});
